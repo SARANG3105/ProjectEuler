ce55096e2651a461c654b24250a57a38a5439445
Sarangdeep Singh <sarangdeep.singh93@gmail.com>
2017-11-21 00:59:31 -0500

    Largest prime

--- ProjectEuler/src/Prime.java
+++ ProjectEuler/src/Prime.java
@@ -1,19 +1,26 @@
 
 public class Prime {
-
-	static long largePrime(long n) {
-		long i=2;;
-		while(i<n) {
-
-			while(n%i==0) {
-				n=n/i;
-			}i++;
-
-		}return i;
+	static boolean isPrime(long i) {
+		long j=2;
+		while(j<i) {
+			if(i%j==0) {
+				return false;
+			}
+			j++;
+		}return true;
 	}
+		static long largePrime(long n) {
+			int i=2;
+			while(i<n) {
+				if(n%i==0 && isPrime(i)==true) {
+					n=n/i;
+				}
+				i++;
+			}return i;
+		}
 
 	public static void main(String[] args) {
-		long n=   600851475143L;
+		long n=  10L;
 		System.out.println(Prime.largePrime(n));
 
 	}
